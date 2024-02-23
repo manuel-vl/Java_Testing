a9557c9f029b09e0f15149ba8c889c895b6e7790
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-23 00:42:16 -0500

    [FEATURE] Add Stringutil class and first test

--- src/main/java/com/testing_java/java_testing/StringUtil.java
+++ src/main/java/com/testing_java/java_testing/StringUtil.java
@@ -1,4 +1,13 @@
 package com.testing_java.java_testing;
 
 public class StringUtil {
+    public static String repeat(String str, int times){
+        StringBuilder result= new StringBuilder();
+
+        for (int i = 0; i < times; i++) {
+            result.append(str);
+        }
+
+        return result.toString();
+    }
 }
--- src/test/java/com/testing_java/java_testing/StringUtilTest.java
+++ src/test/java/com/testing_java/java_testing/StringUtilTest.java
@@ -1,5 +1,16 @@
 package com.testing_java.java_testing;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 class StringUtilTest {
 
+    @Test
+    public void testRepear(){
+        String result=StringUtil.repeat("hola", 3);
+        Assert.assertEquals("holaholahola", result);
+
+        String result2=StringUtil.repeat("hola",1);
+        Assert.assertEquals("hola", result2);
+    }
 }
